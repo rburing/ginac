29bdf53d3b8b71410e2fdee87dd8d8eecf439ece
Richard Kreckel <kreckel@ginac.de>
2023-08-12 18:17:41 +0200

    Finalize 1.8.7 release.

--- ginac/version.h
+++ ginac/version.h
@@ -30,7 +30,7 @@
 #define GINACLIB_MINOR_VERSION 8
 
 /* Micro version of GiNaC */
-#define GINACLIB_MICRO_VERSION 6
+#define GINACLIB_MICRO_VERSION 7
 
 // GiNaC library version information. It has very little to do with GiNaC
 // version number. In particular, library version is OS dependent. 
@@ -56,7 +56,7 @@
 //  * change matrix inverse to use default argument (twice)
 //  * check for interfaces marked as deprecated
 #define GINAC_LT_CURRENT  12
-#define GINAC_LT_REVISION 5
+#define GINAC_LT_REVISION 6
 #define GINAC_LT_AGE      1
 
 /*
